3351462e734565d2e0a48d49efff0eb8e09c2d99
jgehrett <jacobgehrett@gmail.com>
2021-04-07 14:06:01 -0600

    Couldn't remember if I had already pushed

--- back-end/server.js
+++ back-end/server.js
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://localhost:27017/opal', {
 });
 
 // Create a new item in the museum: takes a title and a path to an image.
-app.post('/api/horizonImages', async (req, res) => {
+app.post('/api/horizonimages', async (req, res) => {
   const image = new hItem({
     name: req.body.name,
     path: req.body.path,
@@ -29,7 +29,7 @@ app.post('/api/horizonImages', async (req, res) => {
   }
 });
 
-app.post('/api/groundImages', async (req, res) => {
+app.post('/api/groundimages', async (req, res) => {
   const image = new gItem({
     name: req.body.name,
     path: req.body.path,
@@ -46,14 +46,14 @@ app.post('/api/groundImages', async (req, res) => {
 // Configure multer so that it will upload to '../front-end/public/images'
 const multer = require('multer')
 const uploadH = multer({
-  dest: '../front-end/public/hImages/',
+  dest: '../front-end/public/himages/',
   limits: {
     fileSize: 10000000
   }
 });
 
 const uploadG = multer({
-  dest: '../front-end/public/gImages/',
+  dest: '../front-end/public/gimages/',
   limits: {
     fileSize: 10000000
   }
@@ -82,7 +82,7 @@ app.post('/api/horizonphotos', uploadH.single('photo'), async (req, res) => {
     return res.sendStatus(400);
   }
   res.send({
-    path: "/hImages/" + req.file.filename
+    path: "/himages/" + req.file.filename
   });
 });
 
@@ -92,7 +92,7 @@ app.post('/api/groundphotos', uploadG.single('photo'), async (req, res) => {
     return res.sendStatus(400);
   }
   res.send({
-    path: "/gImages/" + req.file.filename
+    path: "/gimages/" + req.file.filename
   });
 });
 
@@ -107,7 +107,7 @@ app.get('/api/horizonImages', async (req, res) => {
   }
 });
 
-app.get('/api/groundImages', async (req, res) => {
+app.get('/api/groundimages', async (req, res) => {
   try {
     let items = await gItem.find();
     res.send(items);
@@ -117,7 +117,7 @@ app.get('/api/groundImages', async (req, res) => {
   }
 });
 
-app.delete('/api/horizonImages/:id', async (req, res) => {
+app.delete('/api/horizonimages/:id', async (req, res) => {
   try {
     await hItem.deleteOne({
       _id: req.params.id
@@ -129,7 +129,7 @@ app.delete('/api/horizonImages/:id', async (req, res) => {
   }
 });
 
-app.delete('/api/groundImages/:id', async (req, res) => {
+app.delete('/api/groundimages/:id', async (req, res) => {
   try {
     await gItem.deleteOne({
       _id: req.params.id
@@ -141,7 +141,7 @@ app.delete('/api/groundImages/:id', async (req, res) => {
   }
 });
 
-app.put('/api/horizonImages/:id', async (req, res) => {
+app.put('/api/horizonimages/:id', async (req, res) => {
   try {
     let item = await hItem.findOne({
       _id: req.params.id
@@ -155,7 +155,7 @@ app.put('/api/horizonImages/:id', async (req, res) => {
   }
 });
 
-app.put('/api/groundImages/:id', async (req, res) => {
+app.put('/api/groundimages/:id', async (req, res) => {
   try {
     let item = await gItem.findOne({
       _id: req.params.id
